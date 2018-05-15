a3c2d883c42e14b64fb0ebbada062531119bf977
Tarashish Mishra <sunu0000@gmail.com>
2018-05-15 23:54:35 +0530

    more fixes

--- build-wheels/osx.sh
+++ build-wheels/osx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e -x
 
-cd /io/
-
 brew install cmake
 
 # Install dependencies
@@ -13,8 +11,8 @@ git clone --branch poppler-0.63.0 --depth 1 https://anongit.freedesktop.org/git/
 cd poppler_src/
 cmake -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=none .
 make --silent
-export POPPLER_ROOT=/io/poppler_src/
+export POPPLER_ROOT=$TRAVIS_BUILD_DIR/poppler_src/
 cd ..
 
 # Set shared library paths
-export LD_LIBRARY_PATH="/io/poppler_src/:/io/poppler_src/cpp/"
+export LD_LIBRARY_PATH="$TRAVIS_BUILD_DIR/poppler_src/:$TRAVIS_BUILD_DIR/poppler_src/cpp/"
